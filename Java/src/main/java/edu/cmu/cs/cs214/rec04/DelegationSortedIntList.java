dc141a7f8abc6f771bf8e034a9b353ba98f7ecc6
B232270026 <b232270026@gmail.com>
2025-03-21 16:38:39 +0800

    lab7

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -10,7 +10,56 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class DelegationSortedIntList {
-    // Write your implementation below with API documentation
 
-}
+ public class DelegationSortedIntList implements IntegerList {
+     private final SortedIntList list = new SortedIntList();
+     private int totalAdded = 0;
+ 
+     @Override
+     public boolean add(int value) {
+         totalAdded++;
+         return list.add(value);
+     }
+ 
+     @Override
+     public boolean addAll(IntegerList values) {
+         totalAdded += values.size();
+         // IntegerList дахь addAll методыг тодорхойлох
+         if (values instanceof DelegationSortedIntList delegationSortedIntList) {
+             return list.addAll(delegationSortedIntList.getList());
+         }
+         // Мөн бусад IntegerList хэсэгт хэрэгжүүлж болно
+         return false;
+     }
+ 
+     @Override
+     public boolean remove(int value) {
+         return list.remove(value);
+     }
+ 
+     @Override
+     public boolean removeAll(IntegerList values) {
+         if (values instanceof DelegationSortedIntList delegationSortedIntList) {
+             return list.removeAll(delegationSortedIntList.getList());
+         }
+         return false;
+     }
+ 
+     @Override
+     public int get(int index) {
+         return list.get(index);
+     }
+ 
+     @Override
+     public int size() {
+         return list.size();
+     }
+ 
+     public int getTotalAdded() {
+         return totalAdded;
+     }
+ 
+     public SortedIntList getList() {
+         return list;
+     }
+ }
--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -10,7 +10,56 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class InheritanceSortedIntList {
-    // Write your implementation below with API documentation
+public class InheritanceSortedIntList extends SortedIntList {
+  private int totalAdded = 0;
 
+  @Override
+    public boolean add(int num) {
+    totalAdded++;
+    return super.add(num);
+  }
+
+  @Override
+    public boolean addAll(IntegerList list) {
+    boolean isChanged = false;
+    for (int i = 0; i < list.size(); i++) {
+      boolean added = super.add(list.get(i)); 
+      if (added) {
+        totalAdded++; 
+        isChanged = true;
+      }
+    }
+    return isChanged;
+  }
+
+  @Override
+    public boolean remove(int num) {
+    return super.remove(num);
+  }
+
+  @Override
+    public boolean removeAll(IntegerList list) {
+    boolean isChanged = false;
+    for (int i = 0; i < list.size(); i++) {
+      if (remove(list.get(i))) {
+        isChanged = true;
+      }
+    }
+    return isChanged;
+  }
+
+    @Override
+    public int get(int index) {
+        return super.get(index); 
+    }
+
+    @Override
+    public int size() {
+        return super.size();
+    }
+
+
+    public int getTotalAdded() {
+        return totalAdded;
+    }
 }
